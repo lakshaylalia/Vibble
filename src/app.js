4169020052519cbb0af73e82e0c8f554b06370b9
Lakshay Lalia <lakshaylalia@gmail.com>
2025-05-29 01:07:23 +0530

    Add user, video model and file upload

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
